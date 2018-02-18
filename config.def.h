a4ede2f1743ebe7e88b238901344f5876ca4f781
Marek Bogusovsky <bobogu@protonmail.com>
2018-02-18 16:23:29 +0100

    Changed font, removed border

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-int borderpx = 2;
+char font[] = "DejaVu Sans Mono:size=12:antialias=true:autohint=true";
+int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
